8aae76bc7e7cd4a8061cc9931742cd4d4e462a10
Mariana Sarinho <164221246+marisarinho@users.noreply.github.com>
2024-09-16 09:31:31 -0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -6,3 +6,14 @@ def somar(v):
     
 v=[6,3,8,7,2,5]    
 print(f'{somar(v)}')
+
+#versao inserindo valores 
+
+def somar(v):
+    soma=0
+    for i in v:
+        soma+=i
+    return soma
+    
+v=list(map(int,input().split()))
+print(f'{somar(v)}')
